fb6c1e7075cf2a458fcb2166fef64ea19f30b423
Danilo Farias de Sousa <danilofariasbkv@gmail.com>
2025-06-11 09:14:03 -0300

    comentando as rotas do coordinator por enquanto

--- jbeventos/routes/coordinator.php
+++ jbeventos/routes/coordinator.php
@@ -4,13 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\eventController;
 
 
-//Rotas do coordenador
+/*Rotas do coordenador
 Route::get('/events/new', [eventController::class,'create'])->name('events.create'); //exibe o formulário de eventos
 Route::post('/events', [eventController::class, 'store'])->name('events.store'); //Enviar o formulário para salvar no banco
 Route::get('/eventos/{id}/edit', [eventController::class, 'edit'])->name('events.edit'); //editar evento   
 Route::put('/eventos/{id}', [eventController::class, 'update'])->name('events.update'); //editar evento
 Route::delete('/eventos/{id}', [EventoController::class, 'destroy']); //excluir evento
 
+*/
 
 
 
--- jbeventos/routes/web.php
+++ jbeventos/routes/web.php
@@ -12,6 +12,11 @@ Route::get('/', function () {
 //Rotas dos eventos
 Route::get('/events', [eventController::class,'index'])->name('events.index'); //exibe a lista de eventos
 Route::get('/events/{$id}/', [eventController::class,'show'])->name('events.show');
+Route::get('/events/new', [eventController::class,'create'])->name('events.create'); //exibe o formulário de eventos
+Route::post('/events', [eventController::class, 'store'])->name('events.store'); //Enviar o formulário para salvar no banco
+Route::get('/eventos/{id}/edit', [eventController::class, 'edit'])->name('events.edit'); //editar evento   
+Route::put('/eventos/{id}', [eventController::class, 'update'])->name('events.update'); //editar evento
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']); //excluir evento
 
 
 
