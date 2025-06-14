1d50af2cb44be9ae97f8e81c3579da443ef3ec24
Felipe-Silva07 <felipe.silva200730@gmail.com>
2025-06-14 16:25:36 -0300

    Criação e Atribuição de Model de Evento
    Foi protegido os campos determinado os relacionamentos

--- jbeventos/app/Models/Event.php
+++ jbeventos/app/Models/Event.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'event_name',
+        'event_description',
+        'event_location',
+        'event_start',
+        'event_expired_at',
+        'visible_event',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'event_start' => 'datetime',
+            'event_expired_at' => 'datetime',
+            'visible_event' => 'boolean',
+        ];
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function coordinator() {
+        return $this->belongsTo(Coordinator::class);
+    }
 }
