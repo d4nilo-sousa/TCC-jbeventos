9a15158be98f799d3075fed16f3edff74c30695a
Felipe-Silva07 <felipe.silva200730@gmail.com>
2025-06-14 15:32:44 -0300

    Criação de Model do Evento
    Foi criado o model de evento, com seus atributos e relacioanmentos

--- jbeventos/app/Models/Comment.php
+++ jbeventos/app/Models/Comment.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'comment',
+        'visible_comment',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'visible_comment' => 'boolean',
+        ];
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event() {
+        return $this->belongsTo(Event::class);
+    }
 }
