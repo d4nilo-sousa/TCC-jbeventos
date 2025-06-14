2df1c3a09063a18baaf76eb3cd35c96f2d8182af
Felipe-Silva07 <felipe.silva200730@gmail.com>
2025-06-14 16:03:33 -0300

    Criação e Atribuição do Model de Coordenador
    Foi feito a segurança dos dados juntamtente com o relacionamentos

--- jbeventos/app/Models/Coordinator.php
+++ jbeventos/app/Models/Coordinator.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Coordinator extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'coordinator_type',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'temporary_password' => 'boolean',
+        ];
+    }
+
+    public function events() {
+        return $this->hasMany(Event::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function course() {
+        return $this->belongsTo(Course::class);
+    }
 }
