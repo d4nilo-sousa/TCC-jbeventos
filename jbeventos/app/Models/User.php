5365a210c29f401f41b3bca396ac9ca9550b3569
Felipe-Silva07 <felipe.silva200730@gmail.com>
2025-07-01 19:09:58 -0300

    Integrando Jetstream com Livewire para Autenticação
    Aqui foi integrado um sistema de Autenticação para o controle de login de usuários

--- jbeventos/app/Models/User.php
+++ jbeventos/app/Models/User.php
@@ -6,37 +6,50 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
-
-    // Campos que podem ser preenchidos em massa
     protected $fillable = [
         'name',
         'email',
         'password',
-        'phone_number',
-        'user_icon',
-        'user_banner',
-        'user_type',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
     ];
 
     /**
@@ -48,38 +61,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'phone_number_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
-
-    // Cursos criados pelo usuário
-    public function createdCourses() {
-        return $this->hasMany(Course::class);
-    }
-
-    // Retorna o coordenador associado a este usuário
-    public function coordinatorRole() {
-        return $this->hasOne(Coordinator::class);
-    }
-
-    // Comentários feitos pelo usuário
-    public function userComments() {
-        return $this->hasMany(Comment::class);
-    }
-
-    // Cursos que o usuário está participando
-    // Relação muitos-para-muitos com a tabela pivot 'course_user_follow'
-    // withTimestamps() mantém os timestamps na tabela pivot atualizados automaticamente
-    public function participatingCourses() {
-        return $this->belongsToMany(Course::class, 'course_user_follow')->withTimestamps();
-    }
-
-    // Reações feitas pelo usuário em eventos
-    // Essa relação usa uma tabela pivot com atributos próprios (EventUserReaction)
-    public function eventReactions() {
-        return $this->hasMany(EventUserReaction::class);
-    }
 }
-
-
--- jbeventos/routes/web.php
+++ jbeventos/routes/web.php
@@ -19,3 +19,13 @@ Route::resource('courses', CourseController::class);
 Route::resource('coordinators', CoordinatorController::class);
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
