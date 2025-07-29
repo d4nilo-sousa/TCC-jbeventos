bdbf36bbc48b277e83176f0f4d90d748f324c0eb
LeonardoBPNS <leonardobodini661@gmail.com>
2025-07-29 15:26:46 -0300

    Login(View)
    Mudanças nas cores utilizadas dentro da view Login, mais focado ao vermelho. Além da mudança de tamanho e colocação de alguns elementos. Imagem nova de Login (pode ser só pra exemplo, fácil de mudar)

--- jbeventos/resources/views/auth/login.blade.php
+++ jbeventos/resources/views/auth/login.blade.php
@@ -1,15 +1,17 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <a href="/">
+                <img src="{{ asset('imgs/logoJb.png') }}" alt="Logo" class="w-60 h-auto mx-auto">
+            </a>
         </x-slot>
 
         <x-validation-errors class="mb-4" />
 
         @session('status')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ $value }}
-            </div>
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ $value }}
+        </div>
         @endsession
 
         <form method="POST" action="{{ route('login') }}">
@@ -17,7 +19,7 @@
 
             <div>
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="off" />
             </div>
 
             <div class="mt-4">
@@ -32,14 +34,17 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4 space-x-4">
+            <div class="flex items-center justify-center mt-4 space-x-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
                 @endif
 
-                <x-button>
+            </div>
+
+            <div class="flex items-center justify-center mt-3">
+                <x-button class="bg-red-600 hover:bg-red-700 w-60 h-10">
                     {{ __('Log in') }}
                 </x-button>
             </div>
@@ -48,7 +53,7 @@
         {{-- Link para registrar, separado e centralizado --}}
         <div class="mt-6 text-center">
             <p class="text-sm text-gray-600">
-                Não tem uma conta? 
+                Não tem uma conta?
                 <a href="{{ route('register') }}" class="underline text-sm text-gray-600 hover:text-gray-900">
                     Crie uma agora
                 </a>
--- jbeventos/resources/views/components/authentication-card.blade.php
+++ jbeventos/resources/views/components/authentication-card.blade.php
@@ -3,7 +3,7 @@
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-11 px-10 py-11 bg-white shadow-md overflow-hidden sm:rounded-2xl">
         {{ $slot }}
     </div>
 </div>
