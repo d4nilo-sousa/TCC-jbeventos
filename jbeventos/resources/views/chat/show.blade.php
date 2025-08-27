2016f5362a6460b2a0193a5b694c9e308651e2b2
Danilo Farias de Sousa <danilofariasbkv@gmail.com>
2025-08-27 12:04:03 -0300

    pequena melhoria
    Botão de voltar adicionado

--- jbeventos/resources/views/chat/show.blade.php
+++ jbeventos/resources/views/chat/show.blade.php
@@ -1,5 +1,15 @@
 <x-app-layout>
+    <div class="relative">
+        {{-- Botão de Voltar no canto superior esquerdo --}}
+        <a href="{{ route('dashboard') }}" 
+           class="absolute top-4 left-4 inline-flex items-center px-4 py-2 bg-gray-200 text-gray-800 rounded-lg hover:bg-gray-300 transition shadow">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+            Voltar
+        </a>
 
-    @livewire('chat', ['otherUser' => $otherUser])
-
+        {{-- Chat --}}
+        @livewire('chat', ['otherUser' => $otherUser])
+    </div>
 </x-app-layout>
