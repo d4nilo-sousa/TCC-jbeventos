72c0f36e4dcd4ed45b84bdf9f8914819dff209cf
Felipe-Silva07 <felipe.silva200730@gmail.com>
2025-09-27 10:09:03 -0300

    Arrumando Erros no Campo de Senha
    Aqui fiz uma correção no arquivo de js, para ele ser reutilizado em todas as views de autenticação de um usúario.

--- jbeventos/resources/views/auth/register.blade.php
+++ jbeventos/resources/views/auth/register.blade.php
@@ -95,14 +95,7 @@
                     </x-label>
                 </div>
             @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    href="{{ route('login') }}">
-                    {{ __('Já possui conta?') }}
-                </a>
-
-                <x-button class="ms-4">
+                <x-button class="mt-4">
                     {{ __('Criar Conta') }}
                 </x-button>
             </div>
