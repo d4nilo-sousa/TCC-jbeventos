dcd58a9b3bfe5489b7b46a666311d076201cff9f
Felipe Barbosa da Silva <felipe.silva200730@gmail.com>
2025-10-16 16:22:19 -0300

    ..
    ..

--- jbeventos/resources/views/courses/index.blade.php
+++ jbeventos/resources/views/courses/index.blade.php
@@ -15,14 +15,6 @@
             @endphp
 
             <div class="flex flex-col sm:flex-row items-stretch sm:items-center gap-3 w-full sm:w-auto">
-                {{-- Botão Novo Curso --}}
-                @if ($loggedAdmin)
-                    <a href="{{ route('courses.create') }}"
-                        class="flex items-center justify-center gap-2 bg-stone-800 text-white px-6 py-2 rounded-full shadow-lg hover:bg-stone-700 transition-colors duration-200 text-sm font-medium">
-                        <i class="ph-fill ph-plus text-lg"></i> Novo Curso
-                    </a>
-                @endif
-
                 <div
                     class="flex flex-col sm:flex-row items-center space-y-3 sm:space-y-0 sm:space-x-3 w-full md:w-auto">
                     <form action="{{ route('courses.index') }}" method="GET" class="w-full flex-grow">
